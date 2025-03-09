5d0b9b2c508aa58c25ccf83c3714b47c7000266d
Joshua0000 <caleblim012@gmail.com>
2025-03-09 13:44:52 +0900

    2025/03/09 RoomInfo Entity & DTO

---
+++ src/main/java/com/ljio/winnerownner_webserver/dto/RoomRequestDTO.java
@@ -0,0 +1,10 @@
+package com.ljio.winnerownner_webserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoomRequestDTO {
+    private String ID;
+}
---
+++ src/main/java/com/ljio/winnerownner_webserver/entity/RoomInfo.java
@@ -0,0 +1,52 @@
+package com.ljio.winnerownner_webserver.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "RoomInfo")
+@Data
+public class RoomInfo {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String ID;
+
+    @Column(name = "RoomSettiongID")
+    private String RoomSettingID;
+
+    @Column(name = "HostID")
+    private String HostID;
+
+    @Column(name = "Name")
+    private String Name;
+
+    @Column(name = "PlayerCount")
+    private int PlayerCount;
+
+    @Column(name = "LimitCount")
+    private int LimitCount;
+
+    @Column(name = "Description")
+    private String Description;
+
+    @Column(name = "IsPublic")
+    private boolean IsPublic;
+
+    @Column(name = "Password")
+    private String Password;
+
+    @Column(name = "Status")
+    private String Status;
+
+    @Column(name = "CreatedAt")
+    @CreationTimestamp
+    private LocalDateTime CreatedAt;
+
+
+
+}
