a6ea5c9c1eea2ca9d592cf6d7ff89ddaacf20cc5
Joshua0000 <caleblim012@gmail.com>
2025-03-09 13:02:49 +0900

    2025/03/09 init project

---
+++ src/main/java/com/ljio/winnerownner_webserver/WinnerownnerWebserverApplication.java
@@ -0,0 +1,13 @@
+package com.ljio.winnerownner_webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinnerownnerWebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinnerownnerWebserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljio/winnerownner_webserver/WinnerownnerWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljio.winnerownner_webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinnerownnerWebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
